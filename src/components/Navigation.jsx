c1774535dc07305e197ed84b506753084a07e2e8
Prathamesh Mhetre <prathameshmhetre25@gmail.com>
2025-08-15 18:21:32 +0530

    feat: develop responsive header with navigation links login button

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,65 @@
+import { useState } from "react";
+
 const Navigation = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isNavLinksVisible, setNavLinksVisibility] = useState(false);
+
+  const increaseHeight = () => {
+    if (isExpanded) {
+      setIsExpanded(false);
+      setNavLinksVisibility(false);
+    } else {
+      setIsExpanded(true);
+    }
+  };
+
+  const handleTransitionEnd = (e) => {
+    if (e.propertyName === "height" && isExpanded) {
+      setNavLinksVisibility(true);
+    }
+  };
+
   return (
     <>
-      <div className="nav-button-container">
-        <button id="nav-menu-btn">☰</button>
+      <div
+        className={`nav-button-container ${isExpanded ? "expanded" : ""}`}
+        onTransitionEnd={handleTransitionEnd}
+      >
+        <button id="nav-menu-btn" onClick={increaseHeight}>
+          ☰
+        </button>
+        <nav id="nav-links-container">
+          <a
+            class={`nav-link ${isNavLinksVisible ? "navLinksVisible" : ""}`}
+            href="#corousal"
+          >
+            Home
+          </a>
+          <a
+            class={`nav-link ${isNavLinksVisible ? "navLinksVisible" : ""}`}
+            href="#about"
+          >
+            About Me
+          </a>
+          <a
+            class={`nav-link ${isNavLinksVisible ? "navLinksVisible" : ""}`}
+            href="#achievement"
+          >
+            Achievements
+          </a>
+          <a
+            class={`nav-link ${isNavLinksVisible ? "navLinksVisible" : ""}`}
+            href="#blogs"
+          >
+            Blogs
+          </a>
+          <a
+            class={`nav-link ${isNavLinksVisible ? "navLinksVisible" : ""}`}
+            href="#contact-me"
+          >
+            Contact Me
+          </a>
+        </nav>
         <button id="teacher-login-btn">Teacher Login</button>
       </div>
     </>
