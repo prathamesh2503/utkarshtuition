e79ad989714be27479fe2f3127b8915548544730
Prathamesh Mhetre <prathameshmhetre25@gmail.com>
2025-10-20 18:07:29 +0530

    fix: fetch api change for production

--- src/components/TeacherLogin.jsx
+++ src/components/TeacherLogin.jsx
@@ -29,7 +29,7 @@ const TeacherLogin = () => {
 
     try {
       // Send data to backend
-      const response = await fetch("http://localhost:4000/login", {
+      const response = await fetch("https://utkarshtuition.vercel.app/login", {
         method: "POST", // method to send data to server
         headers: {
           "Content-Type": "application/json", // telling server we are sending JSON
